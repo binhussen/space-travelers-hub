831d047c81fe4b3aa35742f3d9f5761c6866e805
ecekpo <ememekpo@yahoo.com>
2022-09-27 15:52:38 +0100

    Add dispatch mission action

--- src/pages/Mission.js
+++ src/pages/Mission.js
@@ -1,5 +1,16 @@
-/** @format */
-
-const Mission = () => <div>Missions</div>;
-
-export default Mission;
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Mission from '../components/Mission';
+import { getMissions } from '../redux/missions/missions';
+const Missions = () => {
+  useEffect(() => {
+    if (missions.length === 0) {
+      dispatch(getMissions());
+    }
+  }, []);
+  return (
+    <section>
+    </section>
+  );
+};
+export default Missions;
