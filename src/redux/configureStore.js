3f5da6ed1cdbdbfa4fe357e0df0e8a428a6174a6
ecekpo <ememekpo@yahoo.com>
2022-09-27 15:51:18 +0100

    Add configureStore

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,6 @@
-/** @format */
-
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import missionReducer from './missions/missions';
 import rocketReducer from './rockets/rockets';
-
-const rootReducer = combineReducers({ rockets: rocketReducer });
+const rootReducer = combineReducers({ rockets: rocketReducer, missions: missionReducer });
 const store = configureStore({ reducer: rootReducer });
-
 export default store;
