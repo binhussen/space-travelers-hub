1e71377f8d0f0c0d4b2734d2668d5dede6fc9e0c
ecekpo <ememekpo@yahoo.com>
2022-09-27 15:57:05 +0100

    Add display mission column

---
+++ src/components/Mission.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+const Mission = ({ mission }) => {
+  const { id, name, description } = mission;
+  return (
+    <tr>
+      <td>{name}</td>
+      <thead>{description}</thead>
+      <td>
+        <span>Not a member</span>
+      </td>
+      <td>
+        <button
+          type="button"
+        >
+          Join Mission
+        </button>
+      </td>
+    </tr>
+  );
+};
+Mission.propTypes = {
+  rocket: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
+};
+export default Mission;
