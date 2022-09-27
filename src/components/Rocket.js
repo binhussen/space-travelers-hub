df1db45cb994296e8288db91432ca4bab24bf5a1
binhussen <mhdhsna@gmail.com>
2022-09-27 11:37:01 +0300

    Switch badge

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -13,23 +13,30 @@ const Rocket = ({ rocket }) => {
     <div className="flex justify-center items-start py-4 gap-5">
       <img src={image} alt="rocket" className="w-25vw rounded" />
       <div>
-        <h2 className="font-medium text-2xl">{name}</h2>
+        <h2 className="font-medium text-2xl">
+          {name}
+          {reserved && (
+          <span className="ml-2 p-0.5 bg-green-500 text-white text-xs rounded-md align-super">
+            Reserved
+          </span>
+          )}
+        </h2>
         <p className="mt-2 text-lg max-w-45 ">{description}</p>
         {reserved ? (
-          <button
-            className="mt-6 px-3 py-2 bg-sky-500 text-white bottom-0 rounded-md cursor-pointer"
-            type="button"
-            onClick={() => dispatch(toggleReserved(id))}
-          >
-            Reserve Rocket
-          </button>
-        ) : (
           <button
             className="mt-6 px-3 py-2 bg-red-500 text-white bottom-0 rounded-md cursor-pointer"
             type="button"
             onClick={() => dispatch(toggleReserved(id))}
           >
             Cancel Reservation
+          </button>
+        ) : (
+          <button
+            className="mt-6 px-3 py-2 bg-sky-500 text-white bottom-0 rounded-md cursor-pointer"
+            type="button"
+            onClick={() => dispatch(toggleReserved(id))}
+          >
+            Reserve Rocket
           </button>
         )}
       </div>
